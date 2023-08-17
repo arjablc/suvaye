54a80ac86a7cf0d4382d0e4d089c5bb5c3d4a656
arjablc <arjablc@gmail.com>
2023-08-17 15:28:09 +0545

    bottom navigation bar wired

--- lib/views/components/bottom_nav_bar.dart
+++ lib/views/components/bottom_nav_bar.dart
@@ -3,23 +3,43 @@ import 'package:flutter_svg/svg.dart';
 
 import '../../constants/asset_paths.dart';
 
-class CustomBottomNavBar extends StatelessWidget {
+class CustomBottomNavBar extends StatefulWidget {
   const CustomBottomNavBar({
     super.key,
   });
 
   @override
+  State<CustomBottomNavBar> createState() => _CustomBottomNavBarState();
+}
+
+class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
+  int _currentIndex = 1;
+  @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      elevation: 0,
+      currentIndex: _currentIndex,
       backgroundColor: Colors.white,
+      onTap: (value) => setState(() {
+        _currentIndex = value;
+      }),
       iconSize: 24,
-      selectedLabelStyle: TextStyle(fontSize: 12, color: Colors.grey),
+      selectedLabelStyle: const TextStyle(fontSize: 12, color: Colors.grey),
       items: [
         const BottomNavigationBarItem(
             label: "Today", icon: Icon(Icons.calendar_today_outlined)),
         BottomNavigationBarItem(
-            label: "Services",
-            icon: SvgPicture.asset(ImageAssetPath.serviceIcon)),
+          label: "Services",
+          icon: SvgPicture.asset(
+            ImageAssetPath.serviceIcon,
+            colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+            height: 20,
+          ),
+          activeIcon: SvgPicture.asset(
+            ImageAssetPath.serviceIcon,
+            height: 20,
+          ),
+        ),
         const BottomNavigationBarItem(
             label: "Chat", icon: Icon(Icons.chat_bubble_outline_rounded))
       ],
